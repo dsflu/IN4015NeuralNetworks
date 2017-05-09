d0f85904e85dbc743ad79f9b0dcf72e3d4531acc
fredericklu <fredlu@yeah.net>
2017-05-09 11:15:50 +0200

    MLPModel
    MLPModel

--- video_level_models.py
+++ video_level_models.py
@@ -109,9 +109,11 @@ class MLPModel(models.BaseModel):
   def create_model(self, model_input, vocab_size, **unused_params):
       
     net = slim.fully_connected(model_input,128)
+    dropout = tf.layers.dropout(
+      inputs=net, rate=0.4)
 
     output = slim.fully_connected(
-    net, vocab_size, activation_fn=tf.nn.sigmoid,
+    dropout, vocab_size, activation_fn=tf.nn.sigmoid,
     weights_regularizer=slim.l2_regularizer(0.01))
 
     return {"predictions": output}
