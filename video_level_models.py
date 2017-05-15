e8da68da3c5822ebdaca93631314b23b05067c9d
fredericklu <fredlu@yeah.net>
2017-05-15 11:22:58 +0200

    MLP2Layers
    MLP2Layers

--- video_level_models.py
+++ video_level_models.py
@@ -108,7 +108,8 @@ class MLPModel(models.BaseModel):
 
   def create_model(self, model_input, vocab_size, **unused_params):
       
-    net = slim.fully_connected(model_input,128)
+    net = slim.fully_connected(model_input,512)
+    net = slim.fully_connected(net,4096)
     dropout = tf.layers.dropout(
       inputs=net, rate=0.4)
 
